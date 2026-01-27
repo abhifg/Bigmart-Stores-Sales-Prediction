7e3e84ed642ea96604a53169ccf8a4940f60b046
Abhirup Ghosh <abhirup9799@gmail.com>
2026-01-27 22:13:19 +0530

    adding data_ingestion.py

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,11 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] Line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] Line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
 
--- src/logger.py
+++ src/logger.py
@@ -3,9 +3,9 @@ import os
 from datetime import datetime
 
 log_file=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs(logs_path,exist_ok=True)
-log_file_path=os.path.join(logs_path,log_file)
+logs_dir=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_dir,exist_ok=True)
+log_file_path=os.path.join(logs_dir,log_file)
 
 logging.basicConfig(
     filename=log_file_path,
